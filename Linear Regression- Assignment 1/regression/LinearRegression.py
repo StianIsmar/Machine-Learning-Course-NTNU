854af9d5d0c8e54500862d1fb02793f0b69f080e
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-01-23 20:18:23 +0100

    Started working on importing csv

---
+++ Linear Regression- Assignment 1/regression/LinearRegression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.7.2
+import numpy as np
+
+class LinearRegress():
+    def readCsv(self):
+        print("this is the CSV # of rows":)
+        np.loadtxt(open("test.csv", "rb"), delimiter=",", skiprows=1)
+
+        my
+        file = object.myfilePath
