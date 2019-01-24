b50d35ebbc7962b0e434fdccc11227eb74b06033
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-01-24 22:14:48 +0100

    X and Y completed for two features linear regression

--- Linear Regression- Assignment 1/regression/LinearRegression.py
+++ Linear Regression- Assignment 1/regression/LinearRegression.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 import os
 
-class LinearRegress():
+class LinearRegressOneFeature():
     def __init__(self):
         print("constructor")
         dataArray, numOfObs = self.readCsv()
@@ -13,21 +13,14 @@ class LinearRegress():
         os.chdir('../regression')
         cwd = os.getcwd()  # Get the current working directory (cwd)
         files = os.listdir(cwd)  # Get all the files in that directory
-        # print("Files in '%s': %s" % (cwd, files))
-        # Lenth of csv file!! store it somewhere
 
         with open("train_2d_reg_data.csv") as f:
             reader = csv.reader(f)
             counter = 0
             dataArray = []
             for row in reader:
-                # print(' '.join(row))
                 dataArray.append(row)
-                # print(row)
-                # matrix = np.concatenate((matrix, row))
-                # print(row)
                 counter += 1
-            # print(matrix)
             # number of observations in the CSV-file.
             counter = counter - 1
             print(counter, "COUNTEREND")
@@ -38,12 +31,13 @@ class LinearRegress():
     def constructMatrix(self, dataArray, numOfObs):
         # print(numOfObs) # 325 rows of observations!
         dataArray = np.delete(dataArray, 0, axis = 0 )
+
         # M has numOfObs rows and 3 columns!
         M = np.ones((numOfObs, 3))
         # print(M)
 
         # X has numOfObs rows and 3 columns!
-        X = np.zeros((numOfObs, 3))
+        X = np.ones((numOfObs, 3))
         # print (X)
 
         # Y is just 1-d array:
@@ -55,16 +49,15 @@ class LinearRegress():
             Y = np.append(Y, obs[2])
         # Remove the two first elements, they should not be there:
         Y =  np.delete(Y,0)
+        print(Y)
+        
         # *** M: Should have 3 columns, with each having row 1,x1,x2:
-        print
+        place = 0
         for obs in dataArray:
-            print(obs)
-            for row in range(0, len(M)):
-                for col in range(1,3):
-                    print(M[row,col])
-                    M[row,col] = obs[(col-1)]
-        print(M)
-
+            # print(obs)
+            X[place,1]= obs[1]
+            X[place,2] = obs[2]
+            place += 1
 
 
 
@@ -76,4 +69,4 @@ class LinearRegress():
             # print(len(dataArray))
             # print(len(Y))
 
-l1 = LinearRegress()
+l1 = LinearRegressOneFeature()
